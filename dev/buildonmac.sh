40a1d4b9507d5724e99d02e72aa59efe6ba5287f
soulhotel.offline <155501797+soulhotel@users.noreply.github.com>
2025-08-22 08:52:09 -0400

    buildonmac script maintenance

--- dev/buildonmac.sh
+++ dev/buildonmac.sh
@@ -5,7 +5,18 @@ echo -e "\n• • • • • • • • • • • • • • • • • �
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 cd "$PROJECT_ROOT" && echo "Working Dir: $PROJECT_ROOT"
-echo -e "\nMake sure the working directory is project root...\n"
+echo -e "
+Make sure the working directory is project root and you are performing these actions on a mac...
+
+For reference: setting up env on mac includes
+  brew install go, node
+  go install github.com/wailsapp/wails/v2/cmd/wails@latest
+  wails doctor
+  npm install -g vite@3.0.7 # dev version
+  # in wails.json > frontend:install and frontend:build are left blank ""
+  wails build
+  dev/buildonmac.sh
+"
 echo -e "Do you want to convert your APP to DMG [Y/n] "
 read -r dmg_answer
 
@@ -14,12 +25,12 @@ if [[ "$dmg_answer" =~ ^[Yy]$ ]]; then
     WAILS_JSON="$PROJECT_ROOT/wails.json"
     APP_NAME=$(jq -r '.info.productName' "$WAILS_JSON")
     APP_BINARY=$(jq -r '.outputfilename' "$WAILS_JSON")
-    APP_PATH="$BUILD_DIR/bin/${APP_BINARY}.app"
+    APP_PATH="$BUILD_DIR/bin/${APP_NAME}.app"
     OUTPUT_DMG="$BUILD_DIR/bin/${APP_BINARY}.dmg"
     if [ ! -d "$APP_PATH" ]; then
         echo "Error: Cant find app, $APP_PATH" && exit 1
     fi
-    MAIN_BIN="$APP_PATH/Contents/MacOS/gituserChrome"
+    MAIN_BIN="$APP_PATH/Contents/MacOS/$APP_BINARY"
     if [ -f "$MAIN_BIN" ]; then
         chmod +x "$MAIN_BIN"
     else
